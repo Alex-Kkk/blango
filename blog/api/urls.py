470db7cc1111cb3ca83594e4dd0002481200307a
Aleksandr Aleksandr <avkirichenko87@gmail.com>
2022-03-16 11:26:22 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -28,6 +28,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+ 
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
@@ -46,4 +47,9 @@ urlpatterns += [
         name="schema-swagger-ui",
     ),
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ), 
 ]
